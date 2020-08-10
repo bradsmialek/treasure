e0e92e60347c183abef5aabc3111c7a675ab822f
bmyers624 <b.myers624@gmail.com>
2020-08-10 00:48:02 -0700

    updates to random message

--- src/com/treasure/util/RandomMessage.java
+++ src/com/treasure/util/RandomMessage.java
@@ -14,7 +14,20 @@ public class RandomMessage {
     }
     public static String rMessage= null;
 
-    public static String [] messageArr = {"'Take a swig of this here rum, ye scumbag!'","'I found this Gold just laying around...'", "'I'll be taking that Gold ye got there!'", "'*Hiccup* I think me be a bit drunk...'", "'Ahoy ye matey!'", "'Bloody pirates...'", "'Off with ye!'", "'Yo-ho and a bottle of rum!'", "'I heard one of ye shipmates has scurvy...'", "'Have ye seen me eye?'", "'Ever wonder why we have no free will... Uhhh, Oops! I mean Arghh!'"};
+    public static String [] messageArr =
+            {
+            "'Take a swig of this here rum, ye scumbag!'",
+            "'I found this Gold just laying around...'",
+            "'I'll be taking that Gold ye got there!'",
+            "'*Hiccup* I think me be a bit drunk...'",
+            "'Ahoy ye matey!'",
+            "'Bloody pirates...'",
+            "'Off with ye!'",
+            "'Yo-ho and a bottle of rum!'",
+            "'I heard one of ye shipmates has scurvy...'",
+            "'Have ye seen me eye?'",
+            "'Ever wonder why we have no free will... Uhhh, Oops! I mean Arghh!'"
+            };
 
     public static String randomMessageGenerator() throws IOException {
 
